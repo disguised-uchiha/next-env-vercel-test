8109a72e103f2297b925dd1c0dcc0e1f4ecf1043
disguised-uchiha <harsh.boricha2015@gmail.com>
2022-04-16 17:21:59 +0530

    feat: test

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  console.log("Apna env:", process.env)
+  console.log("Apna env:", process.env.NEXT_PUBLIC_TT)
   return (
     <div style={{ height: "100vh", width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-      Apna ENV
+      Apna ENV: {process.env.NEXT_PUBLIC_TT}
     </div>
   )
 }
