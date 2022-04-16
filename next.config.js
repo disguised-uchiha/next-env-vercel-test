ed279e3ba22988293a4a127389a874c9fb7293fb
disguised-uchiha <harsh.boricha2015@gmail.com>
2022-04-16 17:09:50 +0530

    feat: test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 console.log("In next config", process.env.ENV);
 
-const localEnv = dotenv.config({ path: `.env.${process.env.ENV || "development"}` }).parsed;
+const localEnv = dotenv.config({ path: `.env.${process.env.ENV}` }).parsed;
 
 console.log("apna localEnv", JSON.stringify(localEnv));
 
